fc95969ca57f5dbf271dcd40dc7be0fe0159e602
Saif Safwan <saifassafwan@gmail.com>
2018-12-15 13:06:53 +0800

    Added library

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -1,3 +1,14 @@
 function pushPage(page){
     document.querySelector('#mainView').pushPage(page+'.html');
-}   
+}
+
+var login = function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    if (username === 'bob' && password === 'secret') {
+      ons.notification.alert('Congratulations!');
+    } else {
+      ons.notification.alert('Incorrect username or password.');
+    }
+};
