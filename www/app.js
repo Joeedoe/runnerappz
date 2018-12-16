1fd4634adfaf090d44bd5159cba3f305f7ca327d
Joeedoe <johan.sukaini@gmail.com>
2018-12-16 20:19:38 +0800

    login function

--- www/app.js
+++ www/app.js
@@ -1,14 +1,51 @@
+//GLOBAL VARIABLE
+auth = firebase.auth();
+db = firebase.firestore();
+
+userdata = {};
+
+//on ready document
+$(document).ready(function(){
+    checkAuth();
+});
+
+//check user session
+function checkAuth(){
+    auth.onAuthStateChanged(function(authUser){
+        if(authUser){
+            userdata = authUser;
+            pushPage('home');
+        }
+        else{
+            userdata = {};
+            pushPage('intro');
+        }
+    });
+}
+
+//push page to top
 function pushPage(page){
     document.querySelector('#mainView').pushPage(page+'.html');
 }
 
-var login = function() {
+//user login
+function login(){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-  
-    if (username === 'bob' && password === 'secret') {
-      ons.notification.alert('Congratulations!');
-    } else {
-      ons.notification.alert('Incorrect username or password.');
-    }
-};
+    auth.signInWithEmailAndPassword(username, password)
+        .then(function(user) {
+            checkAuth();
+        }, function(error) {
+            console.log(error);
+        });
+}
+
+//user logout
+function logout(){
+    auth.signOut()
+        .then(function(){
+            checkAuth();
+        }, function(error){
+            console.log(error);
+        });
+}
